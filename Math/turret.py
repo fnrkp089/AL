3e955b8d2f87f79dc126d3e3a5ded4c17dc7d2d8
fnrkp089 <fnrkp089@gmail.com>
2021-12-07 17:54:15 +0900

    [211207] 터렛죽어어어어

--- Math/turret.py
+++ Math/turret.py
@@ -1,2 +1,12 @@
 n = int(input())
-x, y, lat = map(int, input().split())  # map으로 숫자 변환
+for _ in range(n):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    distance = ((x1-x2)**2 + (y1-y2)**2) ** 0.5  # 두 원의 거리를 제곱근(원의방정식)
+    if distance == 0 and r1 == r2:  # 두 원이 동심원이고 반지름이 같을 때 = 무한대
+        print(-1)
+    elif abs(r1-r2) == distance or r1 + r2 == distance:  # 내접, 외접일 때 = 1점에서만남
+        print(1)
+    elif abs(r1-r2) < distance < (r1+r2):  # 두 원이 서로다른 두 점에서 만날 때 = 2점에서만남
+        print(2)
+    else:
+        print(0)  # 그 외에
